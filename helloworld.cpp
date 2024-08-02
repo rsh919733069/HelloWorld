0a99642332b11337529799c86b399df61e68c864
aabbccrsh <manlyhang@qq.com>
2024-08-02 22:29:24 +0800

    新建helloworld.cpp文件，写了初始框架

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void sort(int* arr, int size)
+{
+    
+}
+
+int main()
+{
+    cout << "hello world!!!" << endl;
+    int arr[] = {12,352,321,5233,5123,1,3421};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    sort(arr, size);
+    
+    return 0;
+}
